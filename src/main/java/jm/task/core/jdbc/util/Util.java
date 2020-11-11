eb34c18b83d323279ccb50e30bd84823ceb88776
rudikone <mudoda51>
2020-11-12 00:45:55 +0300

    Практическая задача 1.1.4 выполнена

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -39,7 +39,6 @@ public class Util {
                 //settings.put(Environment.SHOW_SQL, "true");
 
                 //settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-
                 //settings.put(Environment.HBM2DDL_AUTO, "create-drop");
                 settings.put(Environment.HBM2DDL_AUTO, "update");
 
